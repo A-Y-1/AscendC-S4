5da6bf9d417c5101e2e997a53f538ed5d31b09dc
ypy <1833224896@qq.com>
2025-04-05 17:08:37 +0800

    update Heaviside

--- Heaviside/op_host/heaviside.cpp
+++ Heaviside/op_host/heaviside.cpp
@@ -3,89 +3,86 @@
 #include "register/op_def_registry.h"
 #include "tiling/platform/platform_ascendc.h"
 
-
 namespace optiling {
-static ge::graphStatus TilingFunc(gert::TilingContext* context)
-{
+static ge::graphStatus TilingFunc(gert::TilingContext* context) {
   const static int BLOCK_SIZE = 32;
   const static int TENSOR_NUM = 3;
-  const static int BUFFER_NUM = 2;
+  static int BUFFER_NUM = 2;
   HeavisideTilingData tiling;
   // get platformInfo
   uint64_t ub_size;
-  auto ascendcPlatform = platform_ascendc::PlatformAscendC(context->GetPlatformInfo());
+  auto ascendcPlatform =
+      platform_ascendc::PlatformAscendC(context->GetPlatformInfo());
   ascendcPlatform.GetCoreMemSize(platform_ascendc::CoreMemType::UB, ub_size);
   ub_size -= 8 * 1024;  //为Select预留的空间
   // get Core Num
   int16_t coreNum = ascendcPlatform.GetCoreNumAiv();
   // dataLength &size
-  uint64_t typeSize = GetSizeByDataType(context->GetInputDesc(0)->GetDataType());
+  uint64_t typeSize =
+      GetSizeByDataType(context->GetInputDesc(0)->GetDataType());
   int32_t blockElem = BLOCK_SIZE / typeSize;
   int32_t totalLength = context->GetInputTensor(0)->GetShapeSize();
-  int32_t totalLengthAligned = (totalLength+blockElem-1) / blockElem * blockElem;
+  int32_t totalLengthAligned =
+      (totalLength + blockElem - 1) / blockElem * blockElem;
 
-  //allocate
+  // allocate
   uint32_t totalBlocks = totalLengthAligned / blockElem;
   uint32_t blockPerCore;
-  while(totalBlocks<coreNum*100&&coreNum>=2) coreNum /= 2;
+  while (totalBlocks < coreNum * 50 && coreNum >= 2) coreNum /= 2;
   blockPerCore = totalBlocks / coreNum;
-  
-  uint8_t nBigCore = totalBlocks %coreNum;
-  uint32_t maxBlockPerIter = ub_size/TENSOR_NUM/BUFFER_NUM/BLOCK_SIZE;
-  uint32_t blockInfo = (blockPerCore<<8) | nBigCore;
-  //printf("blockPerCore=%u coreNum=%d maxBlockPerIter=%d nBigCore=%d\n",blockPerCore, coreNum, maxBlockPerIter, nBigCore);
+  // pack blockInfo
+  uint8_t nBigCore = totalBlocks % coreNum;
+  uint32_t maxBlockPerIter = ub_size / TENSOR_NUM / BUFFER_NUM / BLOCK_SIZE;
+  uint32_t blockInfo = (blockPerCore << 8) | nBigCore;
+  // printf("blockPerCore=%u coreNum=%d maxBlockPerIter=%d
+  // nBigCore=%d\n",blockPerCore, coreNum, maxBlockPerIter, nBigCore);
   // set TILING DATA
   tiling.set_maxBlockPerIter(maxBlockPerIter);
   tiling.set_blockInfo(blockInfo);
   context->SetBlockDim(coreNum);
-  tiling.SaveToBuffer(context->GetRawTilingData()->GetData(), context->GetRawTilingData()->GetCapacity());
+  tiling.SaveToBuffer(context->GetRawTilingData()->GetData(),
+                      context->GetRawTilingData()->GetCapacity());
   context->GetRawTilingData()->SetDataSize(tiling.GetDataSize());
 
   return ge::GRAPH_SUCCESS;
 }
-}
-
+}  // namespace optiling
 
 namespace ge {
-static ge::graphStatus InferShape(gert::InferShapeContext* context)
-{
-    const gert::Shape* x1_shape = context->GetInputShape(0);
-    gert::Shape* y_shape = context->GetOutputShape(0);
-    *y_shape = *x1_shape;
-    return GRAPH_SUCCESS;
+static ge::graphStatus InferShape(gert::InferShapeContext* context) {
+  const gert::Shape* x1_shape = context->GetInputShape(0);
+  gert::Shape* y_shape = context->GetOutputShape(0);
+  *y_shape = *x1_shape;
+  return GRAPH_SUCCESS;
 }
-}
-
+}  // namespace ge
 
 namespace ops {
 class Heaviside : public OpDef {
-public:
-    explicit Heaviside(const char* name) : OpDef(name)
-    {
-        this->Input("input")
-            .ParamType(REQUIRED)
-            .DataType({ge::DT_FLOAT, ge::DT_FLOAT16})
-            .Format({ge::FORMAT_ND, ge::FORMAT_ND})
-            .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND});
-        this->Input("values")
-            .ParamType(REQUIRED)
-            .DataType({ge::DT_FLOAT, ge::DT_FLOAT16})
-            .Format({ge::FORMAT_ND, ge::FORMAT_ND})
-            .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND});
-        this->Output("out")
-            .ParamType(REQUIRED)
-            .DataType({ge::DT_FLOAT, ge::DT_FLOAT16})
-            .Format({ge::FORMAT_ND, ge::FORMAT_ND})
-            .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND});
+ public:
+  explicit Heaviside(const char* name) : OpDef(name) {
+    this->Input("input")
+        .ParamType(REQUIRED)
+        .DataType({ge::DT_FLOAT, ge::DT_FLOAT16})
+        .Format({ge::FORMAT_ND, ge::FORMAT_ND})
+        .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND});
+    this->Input("values")
+        .ParamType(REQUIRED)
+        .DataType({ge::DT_FLOAT, ge::DT_FLOAT16})
+        .Format({ge::FORMAT_ND, ge::FORMAT_ND})
+        .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND});
+    this->Output("out")
+        .ParamType(REQUIRED)
+        .DataType({ge::DT_FLOAT, ge::DT_FLOAT16})
+        .Format({ge::FORMAT_ND, ge::FORMAT_ND})
+        .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND});
 
-        this->SetInferShape(ge::InferShape);
+    // this->SetInferShape(ge::InferShape);
 
-        this->AICore()
-            .SetTiling(optiling::TilingFunc);
-        this->AICore().AddConfig("ascend910b");
-
-    }
+    this->AICore().SetTiling(optiling::TilingFunc);
+    this->AICore().AddConfig("ascend910b");
+  }
 };
 
 OP_ADD(Heaviside);
-}
+}  // namespace ops
--- Heaviside/op_kernel/heaviside.cpp
+++ Heaviside/op_kernel/heaviside.cpp
@@ -5,144 +5,158 @@ const static int BUFFER_NUM = 2;
 const static int BLOCK_SIZE = 32;
 const static int COMPARE_ALIGNED = 256;
 template <typename T>
-class kernelHeaviside
-{
-public:
-    __aicore__ inline kernelHeaviside(){}
-    __aicore__ inline void Init(GM_ADDR input, GM_ADDR values, GM_ADDR out, uint32_t maxBlockPerIter, uint32_t blockInfo, TPipe *pipeIn){
-        this->pipe = pipeIn;
-        int coreIdx = GetBlockIdx();
-        int globalOffsetBlock;
-        int blockPerCore = blockInfo >> 8;
-        int bigCoreBlocks = blockPerCore + 1;
-        uint8_t nBigCore = blockInfo & 0xff;
-        globalOffsetBlock = bigCoreBlocks * coreIdx;
-        if (coreIdx < nBigCore)
-        {
-            this->myBlockNum = bigCoreBlocks;
-        }
-        else{
-            this->myBlockNum = blockPerCore;
-            globalOffsetBlock -= (coreIdx - nBigCore);
-        }
-        this->blockElem = BLOCK_SIZE / sizeof(T);
-        int32_t myElem = blockElem * this->myBlockNum;
-        inGm.SetGlobalBuffer((__gm__ T *)input + globalOffsetBlock * blockElem, myElem);
-        valGm.SetGlobalBuffer((__gm__ T *)values + globalOffsetBlock * blockElem, myElem);
-        outGm.SetGlobalBuffer((__gm__ T *)out + globalOffsetBlock * blockElem, myElem);
-        //Data may be unable to load into UB
-        int bufferElem;
-        if (myElem < maxBlockPerIter * blockElem)
-        {
-            this->myIter = 1;
-            this->tailBlocks = 0;
-            blockPerIter = myBlockNum;
-            bufferElem = ((myElem * sizeof(T) + COMPARE_ALIGNED-1) /  COMPARE_ALIGNED) * COMPARE_ALIGNED / sizeof(T);                       //为了保证compare的256byte对齐
-        }
-        else
-        {
-            blockPerIter = maxBlockPerIter;
-            this->myIter = (myBlockNum + blockPerIter - 1) / blockPerIter;
-            this->tailBlocks = myBlockNum % blockPerIter;
-            bufferElem = ((maxBlockPerIter * blockElem * sizeof(T)+COMPARE_ALIGNED-1) / COMPARE_ALIGNED) * COMPARE_ALIGNED / sizeof(T);     //为了保证compare的256byte对齐
-            //printf("maxblock %d myblock %d blockPerIter=%d myIter=%d cal=%d fac=%d therefore=%d bufferElem=%d\n", maxBlockPerIter, myBlockNum, blockPerIter, this->myIter, (myBlockNum + blockPerIter - 1), blockPerIter, (myBlockNum + blockPerIter - 1)/blockPerIter, bufferElem);
-        }
-        pipe->InitBuffer(inputQueue, BUFFER_NUM, bufferElem * sizeof(T));
-        pipe->InitBuffer(valuesQueue, BUFFER_NUM, bufferElem*sizeof(T));
-        pipe->InitBuffer(outQueue, BUFFER_NUM, bufferElem*sizeof(T));
-        pipe->InitBuffer(bitBuf,  bufferElem / 8);
-        //printf("MyIdx = %d myIter=%d myblockNum=%d elem=%d\n", coreIdx, myIter, myBlockNum, myElem);
+class kernelHeaviside {
+ public:
+  __aicore__ inline kernelHeaviside() {}
+  __aicore__ inline void Init(GM_ADDR input, GM_ADDR values, GM_ADDR out,
+                              uint32_t maxBlockPerIter, uint32_t blockInfo,
+                              TPipe *pipeIn) {
+    this->pipe = pipeIn;
+    int coreIdx = GetBlockIdx();
+    int globalOffsetBlock;
+    int blockPerCore = blockInfo >> 8;
+    int bigCoreBlocks = blockPerCore + 1;
+    uint8_t nBigCore = blockInfo & 0xff;
+    globalOffsetBlock = bigCoreBlocks * coreIdx;
+    if (coreIdx < nBigCore) {
+      this->myBlockNum = bigCoreBlocks;
+    } else {
+      this->myBlockNum = blockPerCore;
+      globalOffsetBlock -= (coreIdx - nBigCore);
     }
-    __aicore__ inline void process(){
-        if (this->tailBlocks == 0 || this->myIter == 1)
-        {
-            curElem = blockPerIter * blockElem;
-            elemPerIter = curElem;
-            curElem4CMP = ((curElem * sizeof(T) + COMPARE_ALIGNED - 1) / COMPARE_ALIGNED) * COMPARE_ALIGNED / sizeof(T);
-            copyIn(0);
-            compute(0);
-            copyOut(0);
-        }
-        else{
-            curElem = blockPerIter * blockElem;
-            elemPerIter = curElem;
-            curElem4CMP = ((curElem * sizeof(T) + COMPARE_ALIGNED - 1) / COMPARE_ALIGNED) * COMPARE_ALIGNED / sizeof(T);
-            for (int i = 0; i < myIter - 1;i++)
-            {
-                copyIn(i);
-                compute(i);
-                copyOut(i);
-            }
-            curElem = tailBlocks * blockElem;
-            curElem4CMP = ((curElem * sizeof(T) + COMPARE_ALIGNED - 1) / COMPARE_ALIGNED) * COMPARE_ALIGNED / sizeof(T);
-            copyIn(myIter - 1);
-            compute(myIter - 1);
-            copyOut(myIter - 1);
-        }
+    this->blockElem = BLOCK_SIZE / sizeof(T);
+    int32_t myElem = blockElem * this->myBlockNum;
+    inGm.SetGlobalBuffer((__gm__ T *)input + globalOffsetBlock * blockElem,
+                         myElem);
+    valGm.SetGlobalBuffer((__gm__ T *)values + globalOffsetBlock * blockElem,
+                          myElem);
+    outGm.SetGlobalBuffer((__gm__ T *)out + globalOffsetBlock * blockElem,
+                          myElem);
+    // Data may be unable to load into UB
+    if (myElem < maxBlockPerIter * blockElem) {
+      this->myIter = 1;
+      this->tailBlocks = 0;
+      blockPerIter = myBlockNum;
+      bufferElem =
+          ((myElem * sizeof(T) + COMPARE_ALIGNED - 1) / COMPARE_ALIGNED) *
+          COMPARE_ALIGNED / sizeof(T);  //为了保证compare的256byte对齐
+    } else {
+      blockPerIter = maxBlockPerIter;
+      this->myIter = (myBlockNum + blockPerIter - 1) / blockPerIter;
+      this->tailBlocks = myBlockNum % blockPerIter;
+      bufferElem =
+          ((maxBlockPerIter * blockElem * sizeof(T) + COMPARE_ALIGNED - 1) /
+           COMPARE_ALIGNED) *
+          COMPARE_ALIGNED / sizeof(T);  //为了保证compare的256byte对齐
+      // printf("maxblock %d myblock %d blockPerIter=%d myIter=%d cal=%d fac=%d
+      // therefore=%d bufferElem=%d\n", maxBlockPerIter, myBlockNum,
+      // blockPerIter, this->myIter, (myBlockNum + blockPerIter - 1),
+      // blockPerIter, (myBlockNum + blockPerIter - 1)/blockPerIter,
+      // bufferElem);
     }
-    __aicore__ inline void copyIn(int iterIdx){
-        auto inputLocal = inputQueue.AllocTensor<T>();
-        auto valuesLocal = valuesQueue.AllocTensor<T>();
-        DataCopy(inputLocal, inGm[elemPerIter*iterIdx], curElem);
-        DataCopy(valuesLocal, valGm[elemPerIter*iterIdx], curElem);
-        inputQueue.EnQue(inputLocal);
-        valuesQueue.EnQue(valuesLocal);
+    pipe->InitBuffer(inputQueue, BUFFER_NUM, bufferElem * sizeof(T));
+    pipe->InitBuffer(valuesQueue, BUFFER_NUM, bufferElem * sizeof(T));
+    pipe->InitBuffer(outQueue, BUFFER_NUM, bufferElem * sizeof(T));
+    pipe->InitBuffer(bitBuf, bufferElem / 8);
+    // printf("MyIdx = %d myIter=%d myblockNum=%d elem=%d\n", coreIdx, myIter,
+    // myBlockNum, myElem);
+  }
+  __aicore__ inline void process() {
+    if (this->myIter == 1) {  //单次迭代可完成
+      curElem = blockPerIter * blockElem;
+      elemPerIter = curElem;
+      curElem4CMP =
+          ((curElem * sizeof(T) + COMPARE_ALIGNED - 1) / COMPARE_ALIGNED) *
+          COMPARE_ALIGNED / sizeof(T);
+      copyIn(0);
+      compute(0);
+      copyOut(0);
+    } else if (this->tailBlocks) {  //多次迭代且有尾块
+      curElem = blockPerIter * blockElem;
+      elemPerIter = curElem;
+      curElem4CMP =
+          ((curElem * sizeof(T) + COMPARE_ALIGNED - 1) / COMPARE_ALIGNED) *
+          COMPARE_ALIGNED / sizeof(T);
+      for (int i = 0; i < myIter - 1; i++) {
+        copyIn(i);
+        compute(i);
+        copyOut(i);
+      }
+      curElem = tailBlocks * blockElem;
+      curElem4CMP =
+          ((curElem * sizeof(T) + COMPARE_ALIGNED - 1) / COMPARE_ALIGNED) *
+          COMPARE_ALIGNED / sizeof(T);
+      copyIn(myIter - 1);
+      compute(myIter - 1);
+      copyOut(myIter - 1);
+    } else {  //多次迭代无尾块
+      curElem = blockPerIter * blockElem;
+      elemPerIter = curElem;
+      curElem4CMP =
+          ((curElem * sizeof(T) + COMPARE_ALIGNED - 1) / COMPARE_ALIGNED) *
+          COMPARE_ALIGNED / sizeof(T);
+      for (int i = 0; i < myIter; i++) {
+        copyIn(i);
+        compute(i);
+        copyOut(i);
+      }
     }
-    __aicore__ inline void compute(int iterIdx){
-        //printf("curDataRange:%d - %d 4cmp %d\n", localOffset, localOffset + curElem, curElem4CMP);
-        auto inputLocal = inputQueue.DeQue<T>();
-        auto valuesLocal = valuesQueue.DeQue<T>();
-        auto outLocal = outQueue.AllocTensor<T>();
-        //compute heaviside
+  }
+  __aicore__ inline void copyIn(int iterIdx) {
+    auto inputLocal = inputQueue.AllocTensor<T>();
+    auto valuesLocal = valuesQueue.AllocTensor<T>();
+    DataCopy(inputLocal, inGm[elemPerIter * iterIdx], curElem);
+    DataCopy(valuesLocal, valGm[elemPerIter * iterIdx], curElem);
+    inputQueue.EnQue(inputLocal);
+    valuesQueue.EnQue(valuesLocal);
+  }
+  __aicore__ inline void compute(int iterIdx) {
+    // printf("curDataRange:%d - %d 4cmp %d\n", localOffset, localOffset +
+    // curElem, curElem4CMP);
+    auto outLocal = outQueue.AllocTensor<T>();
+    Duplicate(outLocal, (T)0, curElem4CMP);
+    auto inputLocal = inputQueue.DeQue<T>();
+    auto cmpRes = bitBuf.Get<uint8_t>();
+    CompareScalar(cmpRes, inputLocal, (T)0, CMPMODE::LE, curElem4CMP);
+    Select(outLocal, cmpRes, outLocal, (T)1, SELMODE::VSEL_TENSOR_SCALAR_MODE,
+           curElem4CMP);
+    CompareScalar(cmpRes, inputLocal, (T)0, CMPMODE::EQ, curElem4CMP);
+    auto valuesLocal = valuesQueue.DeQue<T>();
+    Select(outLocal, cmpRes, valuesLocal, outLocal,
+           SELMODE::VSEL_TENSOR_TENSOR_MODE, curElem4CMP);
+    outQueue.EnQue(outLocal);
+    inputQueue.FreeTensor(inputLocal);
+    valuesQueue.FreeTensor(valuesLocal);
+  }
+  __aicore__ inline void copyOut(int iterIdx) {
+    auto outLocal = outQueue.DeQue<T>();
+    DataCopy(outGm[elemPerIter * iterIdx], outLocal, curElem);
+    outQueue.FreeTensor(outLocal);
+  }
 
-        auto cmpRes = bitBuf.Get<uint8_t>();
-        Duplicate(outLocal, (T)0, curElem4CMP);
-        CompareScalar(cmpRes, inputLocal, (T)0, CMPMODE::LE, curElem4CMP);
-        Select(outLocal, cmpRes, outLocal,(T)1,  SELMODE::VSEL_TENSOR_SCALAR_MODE, curElem4CMP);
-        // if(iterIdx==1){
-        //     printf("outLocal7746 =%f input7746=%f values7746=%f\n", outLocal.GetValue(7746), inputLocal.GetValue(7746), valuesLocal.GetValue(7746));
-        // }
-        //2nd Selct
-        CompareScalar(cmpRes, inputLocal, (T)0, CMPMODE::EQ, curElem4CMP);
-        Select(outLocal, cmpRes, valuesLocal, outLocal,  SELMODE::VSEL_TENSOR_TENSOR_MODE, curElem4CMP);
-        // if(iterIdx==1){
-        //     printf("2nd select outLocal7746 =%f input7746=%f values7746=%f\n", outLocal.GetValue(7746), inputLocal.GetValue(7746), valuesLocal.GetValue(7746));
-        // }
-        outQueue.EnQue(outLocal);
-        inputQueue.FreeTensor(inputLocal);
-        valuesQueue.FreeTensor(valuesLocal);
-    }
-    __aicore__ inline void copyOut(int iterIdx){
-
-        auto outLocal = outQueue.DeQue<T>();
-        if(iterIdx==1){
-           // printf("copy out outLocal7746 =%f\n", outLocal.GetValue(7746));
-        }
-        DataCopy(outGm[elemPerIter*iterIdx], outLocal, curElem);
-        outQueue.FreeTensor(outLocal);
-    }
-
-private:
-    TPipe *pipe;
-    GlobalTensor<T> inGm;
-    GlobalTensor<T> valGm;
-    GlobalTensor<T> outGm;
-    TQue<QuePosition::VECIN, BUFFER_NUM> inputQueue;
-    TQue<QuePosition::VECIN, BUFFER_NUM> valuesQueue;
-    TQue<QuePosition::VECOUT, BUFFER_NUM> outQueue;
-    TBuf<QuePosition::VECCALC> bitBuf;
-    int32_t myBlockNum;
-    int32_t myIter, tailBlocks, curElem, blockPerIter, curElem4CMP;
-    int32_t blockElem;
-    int32_t elemPerIter;
+ private:
+  TPipe *pipe;
+  GlobalTensor<T> inGm;
+  GlobalTensor<T> valGm;
+  GlobalTensor<T> outGm;
+  TQue<QuePosition::VECIN, BUFFER_NUM> inputQueue;
+  TQue<QuePosition::VECIN, BUFFER_NUM> valuesQueue;
+  TQue<QuePosition::VECOUT, BUFFER_NUM> outQueue;
+  TBuf<QuePosition::VECCALC> bitBuf;
+  int32_t myBlockNum, bufferElem;
+  int32_t myIter, tailBlocks, curElem, blockPerIter, curElem4CMP;
+  int32_t blockElem;
+  int32_t elemPerIter;
 };
 
-extern "C" __global__ __aicore__ void heaviside(GM_ADDR input, GM_ADDR values, GM_ADDR out, GM_ADDR workspace, GM_ADDR tiling)
-{
-    GET_TILING_DATA(tiling_data, tiling);
-    // TODO: user kernel impl
-    TPipe pipe;
-    kernelHeaviside<DTYPE_INPUT> op;
-    op.Init(input, values, out, tiling_data.maxBlockPerIter, tiling_data.blockInfo, &pipe);
-    op.process();
+extern "C" __global__ __aicore__ void heaviside(GM_ADDR input, GM_ADDR values,
+                                                GM_ADDR out, GM_ADDR workspace,
+                                                GM_ADDR tiling) {
+  GET_TILING_DATA(tiling_data, tiling);
+  // TODO: user kernel impl
+  TPipe pipe;
+  kernelHeaviside<DTYPE_INPUT> op;
+  op.Init(input, values, out, tiling_data.maxBlockPerIter,
+          tiling_data.blockInfo, &pipe);
+  op.process();
 }
